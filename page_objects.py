85ca3fbb59c6492aab642312c87942d157b7bdba
david <yuchild@gmail.com>
2020-10-26 17:50:57 -0700

    Updated 10/26/20

--- page_objects.py
+++ page_objects.py
@@ -3,7 +3,7 @@ from time import sleep
 class LoginPage:
     def __init__(self, browser):
         self.browser = browser
-        
+
     def login(self, username, password):
         username_input = self.browser.find_element_by_css_selector("input[name='username']")
         password_input = self.browser.find_element_by_css_selector("input[name='password']")
@@ -11,7 +11,7 @@ class LoginPage:
         username_input.send_keys(username)
         password_input.send_keys(password)
         
-        login_button = broswer.find_element_by_xpath("//button[@type='submit']")
+        login_button = self.browser.find_element_by_xpath("//button[@type='submit']")
         login_button.click()
         
         sleep(5)
@@ -21,5 +21,6 @@ class HomePage:
         self.browser = browser
         self.browser.get('https://www.instagram.com/')
 
-    def go_to_login_pae(self):
+    def go_to_login_page(self):
+        sleep(2)
         return LoginPage(self.browser)
